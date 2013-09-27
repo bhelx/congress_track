8b5c7988dedbc4b2d7ac060c4954ecad1037913c
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-09-26 23:19:51 -0500

    simpler

--- jobs/purge.rb
+++ jobs/purge.rb
@@ -3,5 +3,4 @@ require_relative '../models'
 
 threshold = Date.today - 7 # last week
 
-puts "Deleted: #{VoterVote.all(:voted_on.lt => threshold).count}"
-VoterVote.all(:voted_on.lt => threshold).destroy
+puts VoterVote.all(:voted_on.lt => threshold).destroy
