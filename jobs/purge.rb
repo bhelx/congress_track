828654b056931e68d3238dd3dbdd352d37bf4d0b
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-10-01 13:41:20 -0500

    hold fewer votes so this can stay free

--- jobs/purge.rb
+++ jobs/purge.rb
@@ -1,6 +1,6 @@
 require 'date'
 require_relative '../models'
 
-threshold = Date.today - 7 # last week
+threshold = Date.today - 3 # last 3 days
 
 puts VoterVote.all(:voted_on.lt => threshold).destroy
