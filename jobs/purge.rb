460ea01a99d771bbcd6eaf626f93382138dc29cd
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-12-01 15:41:46 -0600

    fix bugz

--- jobs/purge.rb
+++ jobs/purge.rb
@@ -4,4 +4,3 @@ require_relative '../models'
 threshold = Date.today - 3 # last 3 days
 
 puts VoterVote.all(:voted_on.lt => threshold).destroy
-puts Vote.all(:created_at.lt => threshold).destroy
--- jobs/votes.rb
+++ jobs/votes.rb
@@ -1,11 +1,12 @@
 #Fetch the votes
 
+require 'date'
 require_relative '../models'
 require_relative '../parsers/votes'
 
 latest_vote = Vote.all(:order => [ :created_at.desc ]).first
 
-fetch_time = latest_vote ? latest_vote.created_at : DateTime.new(2013, 9, 16)
+fetch_time = latest_vote ? latest_vote.created_at : DateTime.new - 1
 fetch_time = DateTime.new(ARGV[0]) if ARGV[0]
 
 if VoteParser.fetch_since fetch_time
