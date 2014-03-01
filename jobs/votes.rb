8ce63cbadd9449587a42ed90afd057016031115d
jnwheeler44 <jnwheeler44@gmail.com>
2014-02-28 21:11:18 -0800

    Use DateTime.now - 1 to get yesterday Datetime.new gives an invalid date for the govtrack api

--- jobs/votes.rb
+++ jobs/votes.rb
@@ -6,7 +6,7 @@ require_relative '../parsers/votes'
 
 latest_vote = Vote.all(:order => [ :created_at.desc ]).first
 
-fetch_time = latest_vote ? latest_vote.created_at : DateTime.new - 1
+fetch_time = latest_vote ? latest_vote.created_at : DateTime.now - 1
 fetch_time = DateTime.new(ARGV[0]) if ARGV[0]
 
 if VoteParser.fetch_since fetch_time
